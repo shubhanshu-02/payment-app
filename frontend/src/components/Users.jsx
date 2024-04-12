403501b90e34d0c4811e8c2e5351343047a6b3e8
Shubhanshu <shubhanshu004@gmail.com>
2024-04-12 19:27:58 +0530

    update Users.jsx to use the auth token

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,45 +1,66 @@
-import { useEffect, useState } from "react"
-import { Button } from "./Button"
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
 import { Logo } from "./Logo";
 import axios from "axios";
 
 export const Users = () => {
-    // Replace with backend call
     const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
 
     useEffect(() => {
-        axios
-        .get("http://localhost:3000/api/v1/user/users" + filter)
-        .then(response => {
-            setUsers(response.data.user)
-        })
-    }, [filter])
-    return <>
-        <div className="font-bold mt-6 text-2xl p-2">
-            Users
-        </div>
-        <div className="my-2 px-3">
-            <input type="text" placeholder="Search users..." className="w-full px-2 py-2 border rounded border-slate-200"></input>
-        </div>
-        <div>
-            {users.map(user => <User user={user} />)}
-        </div>
-    </>
+        const token = localStorage.getItem('token');
+        if (token) {
+            const config = {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            };
+
+            axios
+                .get("http://localhost:3000/api/v1/user/users?filter=" + filter, config)
+                .then(response => {
+                    setUsers(response.data.User);
+                })
+                .catch(error => {
+                    console.error('Error fetching users:', error);
+                });
+        }
+        else {
+            console.error('Token not found in local storage');
+        }
+    }, [filter]);
+
+    return (
+        <>
+            <div className="font-bold mt-6 text-2xl p-2">
+                Users
+            </div>
+            <div className="my-2 px-3">
+                <input onChange={e => {
+                    setFilter(e.target.value);
+                }} type="text" placeholder="Search users..." className="w-full px-2 py-2 border rounded border-slate-200"></input>
+            </div>
+            <div>
+                {users.map(user => <User key={user._id} user={user} />)}
+            </div>
+        </>
+    );
 }
 
-function User({user}) {
-    return <div className="flex justify-between">
-        <div className="flex">
-             <Logo label = {`${user.firstName}`} />
-            <div className="flex flex-col justify-center h-ful">
-                <div>
-                    {user.firstName} {user.lastName}
+function User({ user }) {
+    return (
+        <div className="flex justify-between">
+            <div className="flex p-2">
+                <Logo label={`${user.firstName}`} />
+                <div className="flex flex-col justify-center h-full p-2">
+                    <div>
+                        {user.firstName} {user.lastName}
+                    </div>
                 </div>
             </div>
+            <div className="flex flex-col justify-center h-full px-3">
+                <Button label={"Send Money"} />
+            </div>
         </div>
-
-        <div className="flex flex-col justify-center h-full">
-            <Button label={"Send Money"} />
-        </div>
-    </div>
+    );
 }
