cce08cb9c8320e5f2d265ce331d05e5d8e17485e
Shubhanshu <shubhanshu004@gmail.com>
2024-04-13 15:40:19 +0530

    calling balance from backend

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,10 +1,33 @@
-export const Balance = ({ value }) => {
-    return <div className="flex">
+import { useEffect,useState } from "react"
+import axios from "axios";
+
+export const Balance = () => {
+const [balance,setBalance] = useState(0);
+
+
+const token = localStorage.getItem('token');
+if (token) {
+    const config = {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+
+    axios
+        .get("http://localhost:3000/api/v1/account/balance", config)
+        .then(response => {
+            setBalance(response.data.balance.toFixed(2));
+        })
+    }
+
+
+return (<div className="flex">
         <div className="font-bold text-lg">
             Your balance
         </div>
         <div className="font-semibold ml-4 text-lg">
-            Rs {value}
+            Rs {balance}
         </div>
     </div>
+)
 }
